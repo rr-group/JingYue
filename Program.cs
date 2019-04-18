76a7b239b5d5ab9eee5f8f59b7d838410ab4a18f
rrlee <245894279@qq.com>
2019-04-18 23:33:11 +0800

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JingYueHotel.JingSuanBao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("京悦酒店-精算宝!");
+        }
+    }
+}
